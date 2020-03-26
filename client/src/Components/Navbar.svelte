8dece9f8057495e3cfbe17d311dde9e70f0577d3
Hasibul Huda <hasibulhuda1@gmail.com>
2020-03-26 19:59:39 +0600

    :recycle: rewrote the navbar component to be fixed with tailwind

--- client/src/Components/Navbar.svelte
+++ client/src/Components/Navbar.svelte
@@ -2,6 +2,7 @@
 
     let userloggedin;
     let profileurl = `/profile/${localStorage.getItem('user')}`
+    import { Navbar, NavbarBrand } from 'sveltestrap';
 
     if(localStorage.length === 0){
         userloggedin = false;
@@ -12,85 +13,48 @@
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Montserrat:500&display=swap');
-
-*{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    background-color: #24252A;
-}
-
-li, a, button{
-    font-family: "Montserrat", sans-serif;
-    font-weight: 500;
-    font-size: 16px;
-    color: #edf0f1;
-    text-decoration: none;
-}
-
-.header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 30px 10%;
-}
-
-.navLinks{
-    list-style: none;
-}
-
-.navLinks li{
-    display: inline-block;
-    padding: 0px 20px;
-}
-
-.navLinks li a{
-    transition: all 0.3s ease 0s;
-}
-
-.navLinks li a:hover{
-    color: #0088a9
-}
-
-button{
-    padding: 0px 25px;
-    background-color: rgba(0, 126, 169, 1);
-    border: none;
-    border-radius: 50px;
-    cursor: pointer;
-    transition: all 0.03s ease 0s;
-    ;
-}
-
-button:hover{
-    background-color: rgba(0, 126, 169, 0.8);
-}
-
-.cta{
-    float: right;
-}
-
 </style>
 
-<div class="header">
 
-    <nav style="width: -webkit-fill-available">
-    <ul class="navLinks">
-        <li><a href="/">Home</a></li>
-        <li><a href={profileurl}>Profile</a></li>
-        <li><a href="/about">About</a></li>
+
+
+    <div class="flex-col ">
+
+        <nav class="px-8 py-2 fixed flex-row flex-wrap w-screen h-20 bg-gray-800">
+
+    <ul class="flex justify-between mt-4">
+
+        <div class="flex justify-between ml-32 w-1/6">
+
+            <li><a href="/"><span class="font-light text-lg text-gray-200  cursor-pointer hover:text-blue-500 transition duration-500 ease-in-out">Home</span></a></li>
+            <li><a href={profileurl}><span class="font-light text-lg text-gray-200  hover:text-blue-500 transition duration-500 ease-in-out">Profile</span></a></li>
+            <li><a href="/about"><span class="font-light text-lg text-gray-200  hover:text-blue-500 transition duration-500 ease-in-out">About</span></a></li>
+        
+        </div>
+        
+
+        <div class="flex">
+
 
         {#if userloggedin === false}
-        <a class="cta" href="/login"><button>Login</button></a>
+
+            <a class="" href="/login"><button class="bg-orange-500 mr-40 px-4 py-1 rounded-sm text-lg font-thin hover:bg-orange-400 transition duration-500 ease-in-out">Login</button></a>
         {:else}
-        <a class="cta" href="/"><button on:click={()=>{
-            localStorage.removeItem('user')
-            localStorage.removeItem('userdata')
-        }}>Logout</button></a>
+            <a class="" href="/"><button on:click={()=>{
+                localStorage.removeItem('user')
+                localStorage.indexremoveItem('userdata')
+        }} class="bg-blue-500 mr-40 px-4 py-1 rounded-sm text-lg font-thin hover:bg-blue-400 transition duration-500 ease-in-out">Logout</button></a>
+
         {/if}
+        
+        </div>
+    
     </ul>
+    
+    </nav>
+
+    <div class=" block h-20 bg-orange-500 w-full">
+    </div>
 
     
-</nav>
-
-</div>
+    </div>
